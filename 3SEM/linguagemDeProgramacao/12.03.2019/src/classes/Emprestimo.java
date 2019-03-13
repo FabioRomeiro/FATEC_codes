1d222bc8333782fc7786791bca64bdc56ec36f43
FabioRomeiro <flromeiroc@gmail.com>
2019-03-12 22:56:45 -0300

    Implementacao de classes de acordo com a imagem

---
+++ 3SEM/linguagemDeProgramacao/12.03.2019/src/classes/Emprestimo.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class Emprestimo {
+    private String dataEmprestimo;
+    private String dataDevolucao;
+
+    public String getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public String getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+}
---
+++ 3SEM/linguagemDeProgramacao/12.03.2019/src/classes/Obra.java
@@ -0,0 +1,68 @@
+package classes;
+
+public class Obra {
+
+    private String genero;
+    private String idioma;
+    private String midia;
+    private String nome;
+    private String anoPublicacao;
+    private String autor;
+    private String editora;
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getMidia() {
+        return midia;
+    }
+
+    public void setMidia(String midia) {
+        this.midia = midia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(String anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+}
