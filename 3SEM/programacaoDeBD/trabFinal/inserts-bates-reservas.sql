a3f8a017db11ead2a21784419a497831ff4c75cc
FabioRomeiro <flromeiroc@gmail.com>
2019-05-31 16:50:41 -0300

    Inserts do trabalho final de programacao de BD

---
+++ 3SEM/programacaoDeBD/trabFinal/inserts-bates-reservas.sql
@@ -0,0 +1,17 @@
+insert into reserva values(1234567890, 12345678901, TO_DATE('01/01/2011', 'dd/mm/yyyy'), '07:00', '15:00');
+insert into reserva values(2345678901, 12345678902, TO_DATE('02/02/2012', 'dd/mm/yyyy'), '08:00', '18:00');
+insert into reserva values(3456789012, 12345678903, TO_DATE('03/03/2013', 'dd/mm/yyyy'), '09:00', '19:00');
+insert into reserva values(4567890123, 12345678904, TO_DATE('04/04/2014', 'dd/mm/yyyy'), '10:00', '20:00');
+insert into reserva values(5678901234, 12345678905, TO_DATE('05/05/2015', 'dd/mm/yyyy'), '11:00', '17:00');
+insert into reserva values(6789012345, 12345678906, TO_DATE('06/06/2016', 'dd/mm/yyyy'), '12:00', '22:00');
+insert into reserva values(7890123456, 12345678907, TO_DATE('07/07/2017', 'dd/mm/yyyy'), '13:00', '26:00');
+insert into reserva values(8901234567, 12345678908, TO_DATE('08/08/2018', 'dd/mm/yyyy'), '14:00', '23:00');
+
+insert into guarda values(1234567890, 101);
+insert into guarda values(2345678901, 102);
+insert into guarda values(3456789012, 103);
+insert into guarda values(4567890123, 104);
+insert into guarda values(5678901234, 201);
+insert into guarda values(6789012345, 201);
+insert into guarda values(7890123456, 301);
+insert into guarda values(8901234567, 401);
---
+++ 3SEM/programacaoDeBD/trabFinal/inserts-bates.sql
@@ -0,0 +1,29 @@
+insert into bates values (12345678901, '08:00:10', '17:00:00', 'Grove Street, 100');
+
+insert into slt_sala_de_trabalho values( 12345678901, 001, 001, 'escritorio');
+insert into slt_sala_de_trabalho values( 12345678901, 002, 002, 'escritorio');
+insert into slt_sala_de_trabalho values( 12345678901, 003, 003, 'banheiro');
+insert into slt_sala_de_trabalho values( 12345678901, 004, 004, 'recepcao');
+insert into slt_sala_de_trabalho values( 12345678901, 005, 005, 'cozinha');
+
+insert into and_andar values(001, 12345678901);
+insert into and_andar values(002, 12345678901);
+insert into and_andar values(003, 12345678901);
+insert into and_andar values(004, 12345678901);
+
+insert into quarto values(101, 100.00, 'Regular', 001);
+insert into quarto values(102, 100.00, 'Regular', 001);
+insert into quarto values(103, 150.00, 'Premium', 001);
+insert into quarto values(104, 200.00, 'Gold', 001);
+insert into quarto values(201, 100.00, 'Regular', 002);
+insert into quarto values(202, 100.00, 'Regular', 002);
+insert into quarto values(203, 150.00, 'Premium', 002);
+insert into quarto values(204, 200.00, 'Gold', 002);
+insert into quarto values(301, 100.00, 'Regular', 003);
+insert into quarto values(302, 100.00, 'Regular', 003);
+insert into quarto values(303, 150.00, 'Premium', 003);
+insert into quarto values(304, 200.00, 'Gold', 003);
+insert into quarto values(401, 100.00, 'Regular', 004);
+insert into quarto values(402, 100.00, 'Regular', 004);
+insert into quarto values(403, 150.00, 'Premium', 004);
+insert into quarto values(404, 200.00, 'Gold', 004);
